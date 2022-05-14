05df66e92fe3c8f0388b7cc520640d5630f65ae2
jokkopucko <joakim.dahlstedt@gmail.com>
2022-05-14 13:55:10 +0200

    Making the AN-179 binary switch properly report when the physical switch is clicked

--- drivers/AN-179/device.ts
+++ drivers/AN-179/device.ts
@@ -3,19 +3,11 @@ import { ZwaveDevice } from "homey-zwavedriver";
 class AN179Device extends ZwaveDevice {
     async onNodeInit() {
       this.log("AN179 ABOUT TO INIT");
-      this.registerReportListener(
-        'NOTIFICATION',
-        'NOTIFICATION_REPORT',
-        (x, y) => {
-            this.log(x);
-            if (x['Notification Type'] === 'Access Control') {
-               this.setCapabilityValue('alarm_contact', x.Event === 22);
-            }
-            if (x['Notification Type'] === 'Home Security') {
-                this.setCapabilityValue('alarm_tamper', x.Event != 0);
-            }
-        }
-      );
+      this.registerReportListener('SWITCH_BINARY', 'SWITCH_BINARY_REPORT', (rawReport, parsedReport) => {
+        const onOffString = rawReport['Value'];
+        this.log('SWITCH BINARY: ' + onOffString);
+        this.setCapabilityValue('onoff', onOffString.startsWith('on'));
+      });
       this.registerCapability('onoff', 'BASIC');
       this.log("all done");
   }
